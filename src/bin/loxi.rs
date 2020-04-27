94cfc1b94c2eea63391c032a27c66a9a4366cda8
George Kaplan <george@georgekaplan.xyz>
2020-04-27 03:11:29 -0400

    chapter 15: arithmetic opcodes

--- src/bin/loxi.rs
+++ src/bin/loxi.rs
@@ -1,8 +1,19 @@
+use lox_lang::*;
+
 fn main() {
-    let mut chunk = lox_lang::Chunk::new("script");
+    let mut chunk = Chunk::new("script");
 
-    chunk.write_constant(lox_lang::Value(1.25), 123);
-    chunk.write(lox_lang::Op::Return, 123);
+    chunk.write_constant(Value(1.2), 123);
+    chunk.write_constant(Value(3.4), 123);
 
-    println!("{:?}", chunk);
+    chunk.write(Op::Add, 123);
+
+    chunk.write_constant(Value(5.6), 123);
+
+    chunk.write(Op::Divide, 123);
+    chunk.write(Op::Negate, 123);
+    chunk.write(Op::Return, 123);
+
+    let mut vm = VM::default();
+    vm.interpret(chunk).unwrap()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,97 @@
 use std::fmt;
 
+pub struct VM {
+    stack: Vec<Value>,
+}
+
+impl Default for VM {
+    fn default() -> Self {
+        Self { stack: Vec::new() }
+    }
+}
+
+impl VM {
+    pub fn interpret(&mut self, chunk: Chunk) -> Result<(), Error> {
+        self.run(chunk)
+    }
+
+    fn run(&mut self, chunk: Chunk) -> Result<(), Error> {
+        for (_index, instruction) in chunk.code.iter().enumerate() {
+            {
+                #![cfg(feature = "trace-execution")]
+                // print stack before operation
+                eprint!("          ");
+                for value in &self.stack {
+                    eprint!("[ {} ]", value);
+                }
+                eprintln!();
+
+                // print operation and arguments
+                eprintln!("{:1$x}", chunk, _index);
+            }
+
+            match instruction {
+                Op::Constant(index) => {
+                    let constant = chunk.read_constant(*index as usize);
+                    self.stack.push(constant);
+                }
+                Op::ConstantLong(index) => {
+                    let constant = chunk.read_constant(*index as usize);
+                    self.stack.push(constant);
+                }
+                Op::Add => {
+                    let b = self.pop()?;
+                    let a = self.pop()?;
+                    self.stack.push(a.plus(b)?);
+                }
+                Op::Subtract => {
+                    let b = self.pop()?;
+                    let a = self.pop()?;
+                    self.stack.push(a.minus(b)?);
+                }
+                Op::Multiply => {
+                    let b = self.pop()?;
+                    let a = self.pop()?;
+                    self.stack.push(a.times(b)?);
+                }
+                Op::Divide => {
+                    let b = self.pop()?;
+                    let a = self.pop()?;
+                    self.stack.push(a.divided_by(b)?);
+                }
+                Op::Negate => {
+                    let value = self.pop()?;
+                    self.stack.push(value.negate()?);
+                }
+                Op::Return => {
+                    println!("{}", self.stack.pop().unwrap());
+                    break;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn pop(&mut self) -> Result<Value, Error> {
+        self.stack.pop().ok_or(Error::StackEmpty)
+    }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    StackEmpty,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "lox error")
+    }
+}
+
+impl std::error::Error for Error {}
+
+#[derive(Clone)]
 pub struct Value(pub f64);
 
 impl fmt::Display for Value {
@@ -8,19 +100,57 @@ impl fmt::Display for Value {
     }
 }
 
+impl Value {
+    fn plus(mut self, other: Self) -> Result<Self, Error> {
+        self.0 += other.0;
+        Ok(self)
+    }
+
+    fn minus(mut self, other: Self) -> Result<Self, Error> {
+        self.0 -= other.0;
+        Ok(self)
+    }
+
+    fn times(mut self, other: Self) -> Result<Self, Error> {
+        self.0 *= other.0;
+        Ok(self)
+    }
+
+    fn divided_by(mut self, other: Self) -> Result<Self, Error> {
+        self.0 /= other.0;
+        Ok(self)
+    }
+
+    fn negate(mut self) -> Result<Self, Error> {
+        self.0 *= -1.0;
+        Ok(self)
+    }
+}
+
+#[derive(Clone)]
 pub enum Op {
     Constant(u8),
     ConstantLong(u16),
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Negate,
     Return,
 }
 
 impl Op {
-    fn disassemble(&self, chunk: &Chunk, f: &mut fmt::Formatter) -> fmt::Result {
+    fn disassemble<W: fmt::Write>(&self, chunk: &Chunk, f: &mut W) -> fmt::Result {
         match self {
             Self::Constant(index) => write!(f, "CONSTANT\t{}", chunk.constants[*index as usize]),
             Self::ConstantLong(index) => {
                 write!(f, "CONSTANT_LONG\t{}", chunk.constants[*index as usize])
             }
+            Self::Add => write!(f, "ADD"),
+            Self::Subtract => write!(f, "SUBTRACT"),
+            Self::Multiply => write!(f, "MULTIPLY"),
+            Self::Divide => write!(f, "DIVIDE"),
+            Self::Negate => write!(f, "NEGATE"),
             Self::Return => write!(f, "RETURN"),
         }
     }
@@ -37,39 +167,6 @@ pub struct Chunk {
     constants: Vec<Value>,
     code: Vec<Op>,
     lines: Vec<LineRecord>,
-}
-
-impl fmt::Debug for Chunk {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "== {} ==", self.name)?;
-
-        let mut lines = self.lines.iter();
-        let mut line;
-        let mut line_count = 0; // immediately hooks into advancement below
-        for (idx, op) in self.code.iter().enumerate() {
-            write!(f, "{:04} ", idx)?;
-
-            if line_count == 0 {
-                // advance
-                if let Some(LineRecord { line: l, count: c }) = lines.next() {
-                    line = l;
-                    line_count = *c;
-                } else {
-                    unreachable!("Should not run out of lines before running out of instructions.");
-                }
-
-                write!(f, "{:4} ", line)?;
-            } else {
-                write!(f, "   | ")?;
-            }
-
-            line_count -= 1;
-
-            op.disassemble(self, f)?;
-            writeln!(f)?;
-        }
-        Ok(())
-    }
 }
 
 impl Chunk {
@@ -122,5 +219,87 @@ impl Chunk {
     fn add_constant(&mut self, value: Value) -> usize {
         self.constants.push(value);
         self.constants.len() - 1
+    }
+
+    fn disassemble_instruction<W: fmt::Write>(&self, index: usize, f: &mut W) -> fmt::Result {
+        write!(f, "{:04} ", index)?;
+
+        let (line, is_first) = self.find_line(index);
+
+        if is_first {
+            write!(f, "{:4} ", line)?;
+        } else {
+            write!(f, "   | ")?;
+        }
+
+        self.code[index].disassemble(self, f)
+    }
+
+    fn find_line(&self, instruction_index: usize) -> (usize, bool) {
+        let mut line_num = 1;
+        let mut is_first = true;
+
+        let mut idx_counter = 0;
+        'outer: for LineRecord { line, count } in &self.lines {
+            line_num = *line;
+            is_first = true;
+
+            for _ in 0..*count {
+                if idx_counter == instruction_index {
+                    break 'outer;
+                }
+
+                idx_counter += 1;
+                is_first = false;
+            }
+        }
+
+        (line_num, is_first)
+    }
+
+    fn read_constant(&self, index: usize) -> Value {
+        self.constants[index].clone()
+    }
+}
+
+impl fmt::Display for Chunk {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "== {} ==", self.name)?;
+
+        let mut lines = self.lines.iter();
+        let mut line;
+        let mut line_count = 0; // immediately hooks into advancement below
+        for (idx, op) in self.code.iter().enumerate() {
+            write!(f, "{:04} ", idx)?;
+
+            if line_count == 0 {
+                // advance
+                if let Some(LineRecord { line: l, count: c }) = lines.next() {
+                    line = l;
+                    line_count = *c;
+                } else {
+                    unreachable!("Should not run out of lines before running out of instructions.");
+                }
+
+                write!(f, "{:4} ", line)?;
+            } else {
+                write!(f, "   | ")?;
+            }
+
+            line_count -= 1;
+
+            op.disassemble(self, f)?;
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+/// A terrible, shameful, but ultimately necessary thing to do
+impl fmt::LowerHex for Chunk {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let index = f.width().unwrap_or_default();
+
+        self.disassemble_instruction(index, f)
     }
 }
